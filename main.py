e859d2e3af3ac775c12753d051ffaa510f3fae8f
y.yacenko <face1nSn0w>
2020-05-03 19:56:45 +0300

    add start server

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
     return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run()
