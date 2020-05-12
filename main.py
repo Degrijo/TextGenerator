6487fd244d15119021339db4d9a0788cd4fb941e
y.yacenko <face1nSn0w>
2020-05-12 12:21:14 +0300

    working getting text

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask
+
+from text_generation import get_text
+
+
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    return 'Hello, World!'
+    return get_text()
 
 
 if __name__ == '__main__':
